7597955a7d602c3975a9be2d0f69d3e98176b516
github username <newmanbradm@gmail.com>
2019-02-04 11:54:12 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,13 @@
 // Code Invitation Component Here
+import React from 'react';
+
+const Invitation = props => {
+    return (
+        <div>
+            <h1>{`You've been invited!`}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+export default Invitation;
